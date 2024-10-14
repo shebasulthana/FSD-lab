8edeaef2e0d3fab7dd424079738bd7b7383fa95b
Sheba Sulthana <98817686+shebasulthana@users.noreply.github.com>
2024-10-14 09:16:06 +0530

    Add files via upload

---
+++ WEEK-9/WEEK-8/container.php
@@ -0,0 +1,28 @@
+</head>
+<body class="">
+<div role="navigation" class="navbar navbar-default navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button data-target=".navbar-collapse" data-toggle="collapse" 
+          class="navbar-toggle" type="button">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a href="http://www.phpzag.com" class="navbar-brand">
+          Vifert Jenuben Daniel V - CH.EN.U4AIE21061</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="http://www.phpzag.com">Home</a></li>
+           
+          </ul>
+         
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+	
+	<div class="container" style="min-height:500px;">
+	<div class=''>
+	</div>
